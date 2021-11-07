67c9370212328b5920b5da3f51042e7ec3ad5980
Group1 <Group1@gmail.com>
2021-11-07 08:37:47 +0000

    Group1 1st commit

---
+++ Group1Act1.py
@@ -0,0 +1,6 @@
+prelim = float(input("Enter Prelim Grade: "))
+midterm = float(input("Enter Midterm Grade: "))
+semi = float(input("Enter Semifinal Grade: "))
+final = float(input("Enter Final Grade: "))
+average = ((prelim+midterm+semi+final)/4)
+print(average)
